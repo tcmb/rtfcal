f1dbd827eb2cded7cf7c95c7c70f2e6616374c13
Florian Ilgenfritz <flo@t21n.net>
2018-02-06 17:52:52 +0100

    totally not robust parsing of weekday, date, and dist from home

--- parse_data.py
+++ parse_data.py
@@ -2,10 +2,17 @@ from bs4 import BeautifulSoup
 
 
 def pretty_print(rtf_dict):
-    print rtf_dict
+    print unicode(rtf_dict) + '\n'
 
 def string_format(str):
     return str.strip()
+
+def get_date_and_distance(cell):
+    day_and_date = cell.contents[0]
+    dist_from_home = cell.contents[2]
+    weekday, date = day_and_date.split()
+    return weekday[:-1], date, dist_from_home[1:-1]
+
 
 with open("Termine.html") as fp:
     soup = BeautifulSoup(fp, 'lxml')
@@ -19,9 +26,13 @@ for e in results:
     rtf_link = e.attrs.get('href')
     rtf_cells = e.find_all('div', class_='zelle')
 
+    weekday, date, dist_from_home = get_date_and_distance(rtf_cells[1])
+
     rtf_attributes = {
         'rtf_type': rtf_cells[0].find('div', class_='tooltip').string,
-        'rtf_date_and_distance': rtf_cells[1].string,
+        'rtf_weekday': weekday,
+        'rtf_date': date,
+        'rtf_dist_from_home': dist_from_home,
         'rtf_name': rtf_cells[2].string,
         'rtf_lengths': rtf_cells[3].string,
         'rtf_club': rtf_cells[4].string,
