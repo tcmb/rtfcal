7c19a1776d199eea0c77c3c05e47f2fe53709809
Florian Ilgenfritz <flo@t21n.net>
2018-10-25 14:00:30 +0200

    adding uwsgi ini file and upload script

---
+++ upload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -auvc --delete  --exclude="*.sock" --exclude="*.pyc" --exclude=".git" --exclude=".idea" --exclude=".elasticbeanstalk" 
+--exclude=".gitignore" ./ flo@rtfcal.de:~/rtfcal/
