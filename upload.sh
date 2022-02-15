3cbad0fa4160197079d01eafafa7ed10277f4961
Florian Ilgenfritz <flo@t21n.net>
2022-02-15 11:48:54 +0100

    removing old deployment files

--- upload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync -auvc --delete  --exclude="*.sock" --exclude="*.pyc" --exclude=".git" --exclude=".idea" \
---exclude=".elasticbeanstalk" --exclude=".gitignore" ./ flo@rtfcal.de:~/rtfcal/
